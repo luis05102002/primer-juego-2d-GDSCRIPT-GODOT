fbe5d27f742eeff5b3721beec28798460f67625c
luis05102002 <97956404+luis05102002@users.noreply.github.com>
2026-02-06 11:27:31 +0100

    seguimos actualizando el juego

---
+++ primer_juego_2026-02-03_12-26-31/proyecto/escenas/enemigo/enemigo.gd
@@ -0,0 +1,25 @@
+extends  CharacterBody2D
+
+const PigRun= 90
+const Gravedad = 98
+func _ready():
+	velocity.x = -PigRun
+	$AnimatedSprite2D.play("run")
+	
+func _physics_process(delta):
+	velocity.y += Gravedad
+	
+	if is_on_wall():
+		if !$AnimatedSprite2D.flip_h:
+			velocity.x= PigRun
+		else:
+			velocity.x= -PigRun
+	
+	if velocity.x<0:
+		$AnimatedSprite2D.flip_h= false
+		
+	elif velocity.x>0:
+		$AnimatedSprite2D.flip_h= true
+		
+		
+	move_and_slide()
